73a9d5a3a9221037f2fb365157210211079ad7e5
Jared Reed <42556502+10779692@users.noreply.github.com>
2018-10-08 22:37:19 -0600

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+        <app-header></app-header>
+        <hr>
+            <app-servers></app-servers>
+        <hr>
+        <app-footer></app-footer>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    import Footer from './components/Footer.vue';
+    import Servers from './components/Servers.vue';
+    import ServerDetails from './components/ServerDetails.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
