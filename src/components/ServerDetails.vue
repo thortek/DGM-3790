cc1464fbf1a47f5567e5014e4e391995612f46e8
Jared Reed <42556502+10779692@users.noreply.github.com>
2018-10-09 12:42:19 -0600

    Create ServerDetails.vue

---
+++ src/components/ServerDetails.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div class="">
+        <p>Server Details are currently not updated</p>
+    </div>
+
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid red;
+    }
+</style>
