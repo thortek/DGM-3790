372c3441b0081a5ed7604c0818f408841578cd76
Jared Reed <42556502+10779692@users.noreply.github.com>
2018-10-08 22:48:23 -0600

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,16 @@
 <template>
-      <div class="lines">
-    <h3>Single Lines of Text Added</h3>
-    <div class="w3-border">
-  <div class="w3-blue" style="height:35px;" :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
-      {{ quoteCount }} / {{ maxQuotes }}
-  </div>
-</div>
-<p>To delete a line, simply click on the that line.</p>
-    </div>
+    
+        
+            <header>
+                <h1>Server Status</h1>
+            </header>
+        
+    
 </template>
+
 <script>
-export default {
-    props: ['quoteCount', 'maxQuotes']
-}
 </script>
 
 <style>
-.w3-blue {
-    padding-left: 20px;
-    padding-top: 6px;
-}
+
 </style>
