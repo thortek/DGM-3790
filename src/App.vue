e605ce2ed90a9ab082e44556d0a89e19a5efdc50
Jared Reed <42556502+10779692@users.noreply.github.com>
2018-10-09 12:47:46 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr>
-        <div class="row">
-            <servers></servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
-    </div>
+   <app-servers></app-servers>
 </template>
 
 <script>
-    import Header from './components/Shared/Header.vue';
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue';
 
-    export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
-    }
 </script>
 
 <style>
