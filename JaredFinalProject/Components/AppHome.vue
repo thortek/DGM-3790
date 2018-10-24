a19dd1f70563561aa82c4213274e9f5f480b3d10
Jared Reed <42556502+10779692@users.noreply.github.com>
2018-10-24 17:32:08 -0600

    Create AppHome.vue

---
+++ JaredFinalProject/Components/AppHome.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <app-toolbar></app-toolbar>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import AppFooter from './AppFooter'
+import AppToolbar from './AppToolbar'
+export default {
+    components:{AppFooter,AppToolbar}
+}
+</script>
