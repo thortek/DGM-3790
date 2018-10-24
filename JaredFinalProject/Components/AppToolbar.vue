0cca574c91ddcdb70c7becf6ff26745356e366ea
Jared Reed <42556502+10779692@users.noreply.github.com>
2018-10-24 17:32:31 -0600

    Add files via upload

---
+++ JaredFinalProject/Components/AppToolbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-toolbar color="#313f55" dark>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>J Reed Web Design</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Home</v-btn>
+      <v-btn flat>About</v-btn>
+      <v-btn flat>Portfolio</v-btn>
+      <v-btn flat>Services</v-btn>
+      <v-btn flat>Contact</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
